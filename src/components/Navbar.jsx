f7ab8f74639d3691875e47ed891dd5b755822eef
olivebishop <olivehendrilgen1@gmail.com>
2023-05-12 13:33:59 +0300

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.jpg';
+import { Navbar, Nav } from 'react-bootstrap';
 import './css/Navbar.css';
 
-const Navbar = () => {
+const CustomNavbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-white fixed-top shadow">
-      <Link to="/" className="navbar-brand">
+    <Navbar collapseOnSelect expand="md" bg="white" variant="light" fixed="top">
+      <Navbar.Brand as={Link} to="/">
         <img src={logo} alt="" height="50" />
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-link">About</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/products" className="nav-link">Products</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/services" className="nav-link">Services</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact" className="nav-link">Contact</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto custom-nav">
+          <Nav.Link as={Link} to="/" exact>
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about">
+            About
+          </Nav.Link>
+          <Nav.Link as={Link} to="/products">
+            Products
+          </Nav.Link>
+          <Nav.Link as={Link} to="/services">
+            Services
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contact">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default CustomNavbar;
