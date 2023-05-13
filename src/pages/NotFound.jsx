607ab06558ddfed88c23b15d313c9e556f34bc5a
olivebishop <olivehendrilgen1@gmail.com>
2023-05-13 11:37:06 +0300

    made some changes in NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import error from '../assets/error.gif'
 const NotFoundPage = () => {
   return (
     <div className="not-found-page">
-      <h1>404 Not Found</h1>
-      <img src= {error} alt="404 Not Found" />
+      <Container>
+        <h1 className="text-center">404 Not Found</h1>
+        <div className="text-center">
+          <img
+            src={error}
+            alt="404 Not Found"
+            className="img-fluid"
+            style={{ maxWidth: '100%', height: 'auto' }}
+          />
+        </div>
+      </Container>
     </div>
   );
 };
 
 export default NotFoundPage;
+
