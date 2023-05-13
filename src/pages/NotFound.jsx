de2e452d7e77912b73d99d72fecf30a27adf708a
olivebishop <olivehendrilgen1@gmail.com>
2023-05-13 11:48:43 +0300

    changes done

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import error from '../assets/error.gif'
+import error from '../assets/error.svg'
+import './css/NotFound.css'
 const NotFoundPage = () => {
   return (
     <div className="not-found-page">
       <Container>
-        <h1 className="text-center">404 Not Found</h1>
+        <h1 className="text-center">GO BACK</h1>
+        <h4 className="text-center">PAGE NOT FOUND!</h4>
         <div className="text-center">
           <img
             src={error}
             alt="404 Not Found"
             className="img-fluid"
-            style={{ maxWidth: '100%', height: 'auto' }}
+            style={{ maxWidth: '50%', height: 'auto' }}
           />
         </div>
       </Container>
